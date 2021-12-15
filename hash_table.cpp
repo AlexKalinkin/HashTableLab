ade2fe8fde1a7dbbb196f071cea4655fecc305cf
alex <alexax.kalinkin@gmail.com>
2021-12-15 23:00:58 +0700

    fixed operator [] and ==

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,34 +1,30 @@
 ﻿#include "hash_table.hpp"
 #include <iostream>
 
-int HashTable::hash_function1(const Key& k) const {
-    size_t len = k.length();
-    unsigned int hash = 0;
-    for (int i = 0; i < len; i++) {
-        hash += (hash * first_prime) + k[i];
-    }
-    return hash % capacity_;
-}
-
-int HashTable::hash_function2(const Key& k) const {
-    int hash = hash_fun(second_prime, k);
-
-    if (hash % 2 != 0) return hash;
-    return hash + 1;
-}
-
-int HashTable::hash_fun(int prime_number, const Key& k) {
+unsigned HashTable::hash_fun(const int prime_number, const Key& k) const {
     size_t len = k.length();
     unsigned hash = 0;
     for (int i = 0; i < len; i++) {
         hash += (hash * prime_number) + k[i];
     }
+
     return hash % capacity_;
 }
 
+unsigned HashTable::hash_function1(const Key& k) const {
+    unsigned hash = hash_fun(first_prime, k);
+    return hash % capacity_;
+}
+
+unsigned HashTable::hash_function2(const Key& k) const {
+    unsigned hash = hash_fun(second_prime, k);
+    if (hash % 2 != 0) return hash;
+    return hash + 1;
+}
+
 int HashTable::search(const Key& k) const {
-    int hash1 = hash_function1(k);
-    int hash2 = hash_function2(k);
+    unsigned hash1 = hash_function1(k);
+    unsigned hash2 = hash_function2(k);
     for (int i = 0; i < capacity_; i++) {
         if (bucket[hash1] != nullptr) {
             if (!bucket[hash1]->deleted && bucket[hash1]->key == k) return hash1;
@@ -39,9 +35,15 @@ int HashTable::search(const Key& k) const {
     return -1;
 }
 
+void HashTable::add_bucket(const Key& k, const Value& v, unsigned hash1) {
+    Bucket* a = new Bucket(k, v);
+    bucket[hash1] = a;
+    size_++;
+}
+
 void HashTable::resize() {
     capacity_ *= 2;
-    const Bucket** new_bucket = new const Bucket * [capacity_]();
+    Bucket** new_bucket = new Bucket * [capacity_]();
 
     size_ = 0;
     std::swap(bucket, new_bucket);
@@ -55,7 +57,7 @@ void HashTable::resize() {
     delete[] new_bucket;
 }
 
-HashTable::HashTable() : capacity_(default_capacity), bucket(new const Bucket* [default_capacity]()) {}
+HashTable::HashTable() : capacity_(default_capacity), bucket(new Bucket* [default_capacity]()) {}
 
 HashTable::~HashTable() {
     for (int i = 0; i < capacity_; i++) {
@@ -66,11 +68,14 @@ HashTable::~HashTable() {
 
 HashTable& HashTable::operator=(const HashTable& b) {
     if (this != &b) {
-        // separate method
-        this->~HashTable();
+        for (int i = 0; i < capacity_; i++) {
+            if (bucket[i] != nullptr) delete bucket[i];
+        }
+        delete[] bucket;
+
         size_ = b.size_;
         capacity_ = b.capacity_;
-        bucket = new const Bucket * [b.capacity_]();
+        bucket = new Bucket* [b.capacity_]();
         for (int i = 0; i < capacity_; i++) {
             if (b.bucket[i] != nullptr) {
                 bucket[i] = new Bucket(b.bucket[i]->key, b.bucket[i]->value, b.bucket[i]->deleted);
@@ -80,11 +85,9 @@ HashTable& HashTable::operator=(const HashTable& b) {
     return *this;
 }
 
-HashTable::HashTable(const HashTable& b) : capacity_(b.capacity_), size_(b.size_), bucket(new const Bucket* [b.capacity_]()) {
+HashTable::HashTable(const HashTable& b) : capacity_(b.capacity_), size_(b.size_), bucket(new Bucket* [b.capacity_]()) {
     for (int i = 0; i < capacity_; i++) {
         if (b.bucket[i] != nullptr) {
-//            if (bucket[i] != nullptr) delete bucket[i];
-
             bucket[i] = new Bucket(b.bucket[i]->key, b.bucket[i]->value, b.bucket[i]->deleted);
 
         }
@@ -98,56 +101,52 @@ void HashTable::swap(HashTable& b) {
 }
 
 void HashTable::clear() {
-    // delete
     for (int i = 0; i < capacity_; i++) {
-        if (bucket[i] != nullptr) bucket[i] = nullptr;
+        if (bucket[i] != nullptr) {
+            delete bucket[i];
+            bucket[i] = nullptr;
+        }
     }
     size_ = 0;
 }
 
 bool HashTable::erase(const Key& k) {
-    int hash1 = hash_function1(k);
-    int hash2 = hash_function2(k);
+    unsigned hash1 = hash_function1(k);
+    unsigned hash2 = hash_function2(k);
     for (int i = 0; i < capacity_; i++) {
         if (bucket[hash1] != nullptr) {
             if (bucket[hash1]->key == k) {
-                const_cast<bool&>(bucket[hash1]->deleted) = true;
+                bucket[hash1]->deleted = true;
                 size_--;
                 return true;
-
             }
         }
         else return false;
         hash1 = (hash1 + hash2) % capacity_;
     }
 }
+
 bool HashTable::insert(const Key& k, const Value& v) {
     if (size_ >= capacity_ * 0.75) resize();
-    int hash1 = hash_function1(k);
-    int hash2 = hash_function2(k);
+
+    unsigned hash1 = hash_function1(k);
+    unsigned hash2 = hash_function2(k);
 
     for (int i = 0; i < capacity_; i++) {
         if (bucket[hash1] == nullptr) {
-            const Bucket* a = new const Bucket(k, v);
-            bucket[hash1] = a;
-//            const_cast<bool&>(bucket[hash1]->deleted) = false;
-            size_++;
+            add_bucket(k, v, hash1);
             return true;
         }
 
         else if (bucket[hash1]->deleted) {
             delete bucket[hash1];
-            // add new bucket
-            const_cast<bool&>(bucket[hash1]->deleted) = false;
-            const_cast<Key&>(bucket[hash1]->key) = k;
-            const_cast<Value&>(bucket[hash1]->value) = v;
-            size_++;
+            add_bucket(k, v, hash1);
+            bucket[hash1]->deleted = false;
             return true;
         }
 
         else if (k == bucket[hash1]->key) {
-//            const_cast<Key&>(bucket[hash1]->key) = k;
-            const_cast<Value&>(bucket[hash1]->value) = v;
+            bucket[hash1]->value = v;
             return false;
         }
         hash1 = (hash1 + hash2) % capacity_;
@@ -161,30 +160,39 @@ bool HashTable::contains(const Key& k) const {
 
 Value& HashTable::operator[](const Key& k) {
     int index = search(k);
-    // for empty hashtable:
-    // ht["foo"]; ht["bar"];
     if (index == -1) {
-        // look into how copy works in insert
         Value a("", 0);
-        insert(k, a);
-        return const_cast<Value&>(a);
+        unsigned hash1 = hash_function1(k);
+        unsigned hash2 = hash_function2(k);
 
+        for (int i = 0; i < capacity_; i++) {
+            if (bucket[hash1] == nullptr) add_bucket(k, a, hash1);
+
+            else if (bucket[hash1]->deleted) {
+                delete bucket[hash1];
+                add_bucket(k, a, hash1);
+                bucket[hash1]->deleted = false;
+            }
+
+            return (bucket[hash1]->value);
+        }
     }
-    return const_cast<Value&>(bucket[index]->value);
-}
 
+    return (bucket[index]->value);
+}
+   
 Value& HashTable::at(const Key& k) {
     int index = search(k);
     if (index == -1) throw std::runtime_error("No value for this key");
 
-    return const_cast<Value&>(bucket[index]->value);
+    return (bucket[index]->value);
 }
 
 const Value& HashTable::at(const Key& k) const {
     int index = search(k);
     if (index == -1) throw std::runtime_error("No value for this key");
 
-    return (bucket[index]->value);
+    return const_cast<Value&>(bucket[index]->value);
 }
 
 size_t HashTable::size() const {
@@ -195,14 +203,19 @@ bool HashTable::empty() const {
     return size_ == 0;
 }
 
+bool operator!=(Value& a, Value& b) {
+    if (a.name == b.name && a.age == b.age) return false;
+    return true;
+}
+
 bool operator==(const HashTable& a, const HashTable& b) {
     if (a.size_ != b.size_) return false;
 
     for (int i = 0; i < a.capacity_; i++) {
         if (a.bucket[i] == nullptr) continue;
-        // a -> ("foo", v1)
-        // b -> ("foo", v2)
-        if (!b.contains(a.bucket[i]->key)) return false;
+        int index = b.search(a.bucket[i]->key);
+        if (index == -1) return false;
+        if (a.bucket[i]->value != b.bucket[index]->value) return false;
     }
     return true;
 }
